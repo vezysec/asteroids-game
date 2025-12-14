10da0a3d616d0193ed18df0fe10b47da3f5785bd
Sebastian <sebastian@pop-os.fritz.box>
2025-12-14 14:56:03 +0100

    added circle shape class

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0 
     ### Game Loop
     while True:
         ## log game state for debugging
@@ -25,6 +26,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
